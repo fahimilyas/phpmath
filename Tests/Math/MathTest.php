dd3d2fdcddf6a714fe8b175e10b9c44ff3fac04a
fahimilyas <fahim@inbox.com>
2014-06-09 23:00:21 +1200

    Create MathTest.php

---
+++ Tests/Math/MathTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* Tests the form like encoder
+*
+* @author Kévin Dunglas <dunglas@gmail.com>
+*/
+class MathTest extends \PHPUnit_Framework_TestCase
+{
+    public funtion testAdd()
+    {
+        $this->asserTrue(true);
+    }
+
+}
+
+?>
