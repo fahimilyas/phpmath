3b139507bfea2456341151529cd2ee1c83255506
fahimilyas <fahim@inbox.com>
2014-06-23 20:19:18 +1200

    Update MathTest.php

--- Tests/Math/MathTest.php
+++ Tests/Math/MathTest.php
@@ -8,7 +8,7 @@ class MathTest extends \PHPUnit_Framework_TestCase
 {
     public function testAdd()
     {
-        require_once('../../src/FIMath.php');
+        require_once('/home/travis/build/fahimilyas/phpmath/src/FIMath.php');
         
         $result = FIMath::add(9,3);
         $this->assertEquals($result, 12);
