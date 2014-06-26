60ee1502f73ee995d49bab37ea904c0452386c87
fahimilyas <fahim@inbox.com>
2014-06-27 11:18:34 +1200

    Update MathTest.php

--- Tests/Math/MathTest.php
+++ Tests/Math/MathTest.php
@@ -13,6 +13,13 @@ class MathTest extends \PHPUnit_Framework_TestCase
         $result = FIMath::add(9,3);
         $this->assertEquals($result, 12);
     }
+    public function testSubtract()
+    {
+        require_once('/home/travis/build/fahimilyas/phpmath/src/FIMath.php');
+        
+        $result = FIMath::subtract(9,3);
+        $this->assertEquals($result, 6);
+    }
 }
 
 ?>
